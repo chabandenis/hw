85997c7c257652281fba88b272d4433b69a8e36b
Chaban Denis <dchaban@inno.tech>
2024-11-06 18:21:18 +0700

    прописаны проперти

--- hw01/ex02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw01/ex02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -1,10 +1,13 @@
 package ru.otus.hw.config;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
 @Data
+@PropertySource("application.properties")
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
     // внедрить свойство из application.properties
@@ -12,4 +15,8 @@ public class AppProperties implements TestConfig, TestFileNameProvider {
 
     // внедрить свойство из application.properties
     private String testFileName;
+    public AppProperties(@Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass, @Value("${test.fileName}") String testFileName) {
+        this.testFileName = testFileName;
+        this.rightAnswersCountToPass = rightAnswersCountToPass;
+    }
 }
