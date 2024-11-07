c13152aa68a57788b3c6146c96ea85cdb54a2e4b
Chaban Denis <dchaban@inno.tech>
2024-11-07 16:21:21 +0700

    тест сервиса

--- hw01/ex02/src/test/java/CommonHwTest.java
+++ hw01/ex02/src/test/java/CommonHwTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -9,28 +10,53 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.Assert;
 import ru.otus.hw.Application;
 import ru.otus.hw.config.AppProperties;
+import ru.otus.hw.config.TestConfig;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.CsvQuestionDao;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.service.IOService;
+import ru.otus.hw.service.ResultService;
+import ru.otus.hw.service.ResultServiceImpl;
+import ru.otus.hw.service.StudentService;
+import ru.otus.hw.service.StudentServiceImpl;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
 
 class CommonHwTest {
 
     private static final String CONFIGURATION_ANNOTATION_NAME = "org.springframework.context.annotation.Configuration";
+    private static final int rightAnswersCountToPass = 3;
+    private static final String testFileName = "questions.csv";
 
     @DisplayName("Интеграционный тест класса, читающего вопросы")
     @Test
     void verifyReadQuestionFromFile(){
-        int rightAnswersCountToPass = 3;
-        String testFileName = "questions.csv";
         TestFileNameProvider fileNameProvider = new AppProperties(rightAnswersCountToPass, testFileName);
         QuestionDao questionDao = new CsvQuestionDao(fileNameProvider);
         var questions = questionDao.findAll();
         assertThat(questions.size()).isEqualTo(5);
+    }
+
+    @Test
+    void verifyService(){
+        TestConfig testConfig = new AppProperties(rightAnswersCountToPass, testFileName);;
+
+        IOService ioService;
+        ioService = mock(IOService.class);
+
+        given(ioService.readStringWithPrompt(anyString())).willReturn("XXX");
+
+        StudentService resultService = new StudentServiceImpl(ioService);
+
+        assertThat(resultService.determineCurrentStudent().getFullName()).isEqualTo("XXX XXX");
+
     }
 
     @DisplayName("")
