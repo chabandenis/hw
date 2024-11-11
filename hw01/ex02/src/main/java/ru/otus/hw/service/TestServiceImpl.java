6eb49135778e3275288502f7fbe7474b4ed64adf
Chaban Denis <dchaban@inno.tech>
2024-11-11 23:50:04 +0700

    .

---
+++ hw01/ex02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,43 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+
+        for (var question : questions) {
+            var isAnswerValid = false; // Задать вопрос, получить ответ
+            System.out.println("Вопрос: " + question.text());
+
+            for (var answer : question.answers()) {
+                System.out.println("\t" + answer.text());
+            }
+
+            int enteredAnswer = -1;
+            enteredAnswer = ioService.readIntForRangeWithPrompt(1,
+                    question.answers().size(),
+                    "число:",
+                    "Выберите число в указанном диапазоне");
+            isAnswerValid = question.answers().get(enteredAnswer - 1).isCorrect();
+
+            testResult.applyAnswer(question, isAnswerValid);
+        }
+        return testResult;
+    }
+}
