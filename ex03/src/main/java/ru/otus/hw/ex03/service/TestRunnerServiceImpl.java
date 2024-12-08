f693bf7d935d85f783776b4acf4cf8b530c90d22
Denis Chaban <denisbrd8@gmail.com>
2024-12-08 23:17:04 +0700

    .

--- ex03/src/main/java/ru/otus/hw/ex03/service/TestRunnerServiceImpl.java
+++ ex03/src/main/java/ru/otus/hw/ex03/service/TestRunnerServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class TestRunnerServiceImpl implements TestRunnerService, CommandLineRunner {
+public class TestRunnerServiceImpl implements TestRunnerService/*, CommandLineRunner */{
 
     private final TestService testService;
 
@@ -21,8 +21,11 @@ public class TestRunnerServiceImpl implements TestRunnerService, CommandLineRunn
         resultService.showResult(testResult);
     }
 
+/*
     @Override
     public void run(String... args) throws Exception {
         run();
     }
+
+ */
 }
