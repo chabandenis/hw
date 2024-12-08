ab88c549084a6a433b6b8f73870a20760ddc0ec2
Denis Chaban <denisbrd8@gmail.com>
2024-12-08 23:19:18 +0700

    .

--- ex03/src/main/java/ru/otus/hw/ex03/service/TestRunnerServiceImpl.java
+++ ex03/src/main/java/ru/otus/hw/ex03/service/TestRunnerServiceImpl.java
@@ -1,13 +1,11 @@
 package ru.otus.hw.ex03.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class TestRunnerServiceImpl implements TestRunnerService/*, CommandLineRunner */{
-
+public class TestRunnerServiceImpl implements TestRunnerService/*, CommandLineRunner */ {
     private final TestService testService;
 
     private final StudentService studentService;
