9b36d7ca0e2bb013a85c587d2bee1355e872c81e
Denis Chaban <denisbrd8@gmail.com>
2024-12-09 18:31:35 +0700

    .

--- ex07/src/main/java/ru/otus/hw/ex07/models/Book.java
+++ ex07/src/main/java/ru/otus/hw/ex07/models/Book.java
@@ -3,7 +3,6 @@ package ru.otus.hw.ex07.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.FieldResult;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
