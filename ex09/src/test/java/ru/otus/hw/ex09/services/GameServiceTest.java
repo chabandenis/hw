dcda3f4f22fb69574424672850e8913944dfa7e8
Denis Chaban <denisbrd8@gmail.com>
2024-12-18 00:31:50 +0700

    9

--- ex09/src/test/java/ru/otus/hw/ex09/services/GameServiceTest.java
+++ ex09/src/test/java/ru/otus/hw/ex09/services/GameServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.ex09.dto.InputXYDTO;
 import ru.otus.hw.ex09.mapper.ChessFairMapper;
 import ru.otus.hw.ex09.mapper.FiguraMapper;
 import ru.otus.hw.ex09.mapper.GameMapper;
@@ -32,20 +33,60 @@ class GameServiceTest {
     @Autowired
     private GameService gameService;
 
-//    @Autowired
-//    private GameMapper gameMapper;
+    @Autowired
+    private GameMapper gameMapper;
 
     @Test
     void doStep() {
+
+        var game = gameService.getOne(1L);
+
+        InputXYDTO inputXYDTO = new InputXYDTO();
+        inputXYDTO.setXFirst("A");
+        inputXYDTO.setYFirst("3");
+        inputXYDTO.setXSecond("A");
+        inputXYDTO.setYSecond("4");
+
+        System.out.println(game.getChessFair().getDesk().get(0));
+        System.out.println(game.getChessFair().getDesk().get(1));
+        System.out.println(game.getChessFair().getDesk().get(2));
+        System.out.println(game.getChessFair().getDesk().get(3));
+        System.out.println(game.getChessFair().getDesk().get(4));
+        System.out.println(game.getChessFair().getDesk().get(5));
+        System.out.println(game.getChessFair().getDesk().get(6));
+        System.out.println(game.getChessFair().getDesk().get(7));
+
+        gameService.doStep(game, inputXYDTO);
+
+        var gameAfterStep = gameService.getOne(1L);
+
+        System.out.println("исходная точка было " +
+                game.getChessFair().getDesk().get(5).getArr().get(0).getVal());
+        assertThat(game.getChessFair().getDesk().get(5).getArr().get(0).getVal()).isEqualTo("O");
+
+        System.out.println("исходная точка стало " +
+                gameAfterStep.getChessFair().getDesk().get(5).getArr().get(0).getVal());
+        assertThat(gameAfterStep.getChessFair().getDesk().get(5).getArr().get(0).getVal()).isEqualTo("");
+
+        System.out.println(gameAfterStep.getChessFair().getDesk().get(0));
+        System.out.println(gameAfterStep.getChessFair().getDesk().get(1));
+        System.out.println(gameAfterStep.getChessFair().getDesk().get(2));
+        System.out.println(gameAfterStep.getChessFair().getDesk().get(3));
+        System.out.println(gameAfterStep.getChessFair().getDesk().get(4));
+        System.out.println(gameAfterStep.getChessFair().getDesk().get(5));
+        System.out.println(gameAfterStep.getChessFair().getDesk().get(6));
+        System.out.println(gameAfterStep.getChessFair().getDesk().get(7));
+
+        System.out.println("Конечная точка было " +
+                game.getChessFair().getDesk().get(4).getArr().get(0).getVal());
+        assertThat(game.getChessFair().getDesk().get(4).getArr().get(0).getVal()).isEqualTo("");
+
+        System.out.println("Конечная точка стало " +
+                gameAfterStep.getChessFair().getDesk().get(4).getArr().get(0).getVal());
+        assertThat(gameAfterStep.getChessFair().getDesk().get(4).getArr().get(0).getVal()).isEqualTo("O");
+
     }
 
-    @Test
-    void createEmptyDesk() {
-    }
-
-    @Test
-    void fillFigureOnTheDesk() {
-    }
 
     @Test
     void getOne() {
