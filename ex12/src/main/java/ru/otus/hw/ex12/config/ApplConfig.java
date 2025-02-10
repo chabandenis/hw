79113e512a57b5abf1620e6b8e6224084050a093
Denis Chaban <denisbrd8@gmail.com>
2025-02-10 17:17:53 +0700

    .

--- ex12/src/main/java/ru/otus/hw/ex12/config/ApplConfig.java
+++ ex12/src/main/java/ru/otus/hw/ex12/config/ApplConfig.java
@@ -9,20 +9,6 @@ import reactor.core.scheduler.Schedulers;
 public class ApplConfig {
     private static final int THREAD_POOL_SIZE = 5;
 
-    /*
-    @Bean(destroyMethod = "close")
-    public NioEventLoopGroup eventLoopGroup() {
-        return new NioEventLoopGroup(THREAD_POOL_SIZE,
-                new ThreadFactory() {
-                    private final AtomicLong threadIdGenerator = new AtomicLong(0);
-
-                    @Override
-                    public Thread newThread(@NonNull Runnable task) {
-                        return new Thread(task, "server-thread-" + threadIdGenerator.incrementAndGet());
-                    }
-                });
-    }
-*/
     @Bean
     public Scheduler workerPool() {
         return Schedulers.newParallel("worker-thread", THREAD_POOL_SIZE);
