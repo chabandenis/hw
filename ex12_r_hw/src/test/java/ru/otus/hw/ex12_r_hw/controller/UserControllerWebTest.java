c94c9121eb64e068274b8e92a2116b235d5235c4
Denis Chaban <denisbrd8@gmail.com>
2025-02-19 10:22:00 +0700

    верный комит

--- ex12_r_hw/src/test/java/ru/otus/hw/ex12_r_hw/controller/UserControllerWebTest.java
+++ ex12_r_hw/src/test/java/ru/otus/hw/ex12_r_hw/controller/UserControllerWebTest.java
@@ -6,11 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import ru.otus.hw.ex12_r_hw.config.ApplConfig;
+import ru.otus.hw.ex12_r_hw.dto.UserDto;
+import ru.otus.hw.ex12_r_hw.dto.user.UserCreateDto;
+import ru.otus.hw.ex12_r_hw.dto.user.UserLoginDto;
+import ru.otus.hw.ex12_r_hw.dto.user.UserUpdateDto;
 import ru.otus.hw.ex12_r_hw.models.User;
 import ru.otus.hw.ex12_r_hw.repositories.UserRepository;
 import ru.otus.hw.ex12_r_hw.security.CustomReactiveUserDetailsService;
@@ -18,7 +24,11 @@ import ru.otus.hw.ex12_r_hw.security.MethodSecurityConfiguration;
 import ru.otus.hw.ex12_r_hw.security.SecurityConfiguration;
 import ru.otus.hw.ex12_r_hw.services.UserService;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockUser;
 
 /**
  * Test class for the {@link UserController}
@@ -41,7 +51,6 @@ public class UserControllerWebTest {
     @Autowired
     private WebTestClient webTestClient;
 
-
     @BeforeEach
     public void setup() {
 
@@ -51,89 +60,99 @@ public class UserControllerWebTest {
     public void verif() throws Exception {
     }
 
-/*    @WithMockUser(
-            username = "USER55",
-            authorities = {"ROLE_ADMIN"}
-    )*/
     @Test
     public void getAll() throws Exception {
-
         User user1 = new User(1L, "Aa", "BB", "CC");
         User user2 = new User(2L, "dd", "ee", "ff");
 
         when(userRepository.findAll()).thenReturn(Flux.just(user1, user2));
-        webTestClient.get().uri("/api/user")
+
+        webTestClient
+                .get().uri("/api/user")
+                .exchange()
+                .expectStatus().isUnauthorized();
+
+        webTestClient
+                .mutateWith(mockUser())
+                .get().uri("/api/user")
                 .exchange()
                 .expectStatus().isOk();
     }
 
-    @WithMockUser(
-            username = "USER1",
-            authorities = {"ROLE_ADMIN"}
-    )
     @Test
     public void login() throws Exception {
-/*        String userLoginDto = """
-                {
-                    "login": "",
-                    "password": ""
-                }""";
+        when(userService.findByLogin(any())).thenReturn(Mono.empty());
+        UserLoginDto userLoginDto = new UserLoginDto("user1", "1");
 
-        mockMvc.perform(put("/api/user/login")
-                        .content(userLoginDto)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());*/
+        webTestClient.put()
+                .uri("/api/user/login")
+                .bodyValue(userLoginDto)
+                .exchange()
+                .expectStatus().isUnauthorized();
+
+        webTestClient
+                .mutateWith(mockUser())
+                .put()
+                .uri("/api/user/login")
+                .bodyValue(userLoginDto)
+                .exchange()
+                .expectStatus().isOk();
     }
 
-    @WithMockUser(
-            username = "USER1",
-            authorities = {"ROLE_ADMIN"}
-    )
+    // аутентификация не требуется
     @Test
     public void create() throws Exception {
-/*        String userCreateDto = """
-                {
-                    "name": "",
-                    "login": "",
-                    "password": ""
-                }""";
+        UserCreateDto userCreateDto = new UserCreateDto("user5", "login", "1");
 
-        mockMvc.perform(post("/api/user")
-                        .content(userCreateDto)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());*/
+        // Выполняем POST запрос на создание пользователя
+        webTestClient.post()
+                .uri("/api/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(userCreateDto), UserCreateDto.class)
+                .exchange()
+                .expectStatus().isOk();
     }
 
-    @WithMockUser(
-            username = "USER1",
-            authorities = {"ROLE_ADMIN"}
-    )
+
     @Test
     public void put() throws Exception {
-/*        String userUpdateDto = """
-                {
-                    "name": "",
-                    "login": "",
-                    "password": ""
-                }""";
+        UserUpdateDto userUpdateDto = new UserUpdateDto("Первый Иван Иваныч Иванов", "login", "1");
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/api/user/{0}", "0")
-                        .content(userUpdateDto)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());*/
+        // Выполняем PUT запрос на обновление пользователя
+        webTestClient
+                .put()
+                .uri("/api/user/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(userUpdateDto), UserUpdateDto.class)
+                .exchange()
+                .expectStatus().isUnauthorized();
+
+        // Выполняем PUT запрос на обновление пользователя
+        webTestClient
+                .mutateWith(mockUser())
+                .put()
+                .uri("/api/user/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(userUpdateDto), UserUpdateDto.class)
+                .exchange()
+                .expectStatus().isOk();
     }
 
-    @WithMockUser(
-            username = "USER1",
-            authorities = {"ROLE_ADMIN"}
-    )
     @Test
     public void delete() throws Exception {
-/*        mockMvc.perform(MockMvcRequestBuilders.delete("/api/user/{0}", "0"))
-                .andExpect(status().is(HttpStatus.NO_CONTENT.value()))
-                .andDo(print());*/
+
+        webTestClient
+                .mutateWith(mockUser())
+                .delete()
+                .uri("/api/user/4")
+                .exchange()
+                .expectStatus().isNoContent();
+
+        webTestClient
+                .delete()
+                .uri("/api/user/4")
+                .exchange()
+                .expectStatus().isUnauthorized();
+
     }
 }
