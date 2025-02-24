048ff436ebf92037685ed7bd35effeb5599d5be3
Denis Chaban <denisbrd8@gmail.com>
2025-02-24 14:31:58 +0700

    верный комит

--- ex13/src/main/java/ru/otus/hw/ex13/Ex13_Application.java
+++ ex13/src/main/java/ru/otus/hw/ex13/Ex13_Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ex12_R_Application {
+public class Ex13_Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Ex12_R_Application.class, args);
+        SpringApplication.run(Ex13_Application.class, args);
         System.out.println("Только ресты");
     }
 
