a92e8bb7b7366a285546e3a5b7a751783b4d365b
Denis Chaban <denisbrd8@gmail.com>
2025-02-26 22:48:24 +0700

    верный комит

--- ex00/src/main/java/ru/otus/hw/ex00/dto/repayment/RepaymentDto.java
+++ ex00/src/main/java/ru/otus/hw/ex00/dto/repayment/RepaymentDto.java
@@ -1,11 +1,13 @@
 package ru.otus.hw.ex00.dto.repayment;
 
+import lombok.ToString;
 import lombok.Value;
 
 /**
  * DTO for {@link ru.otus.hw.ex00.models.Repayment}
  */
 @Value
+@ToString
 public class RepaymentDto {
     Long id;
     String numCred;
