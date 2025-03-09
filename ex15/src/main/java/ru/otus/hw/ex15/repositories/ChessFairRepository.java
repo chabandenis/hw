75acd998400739b47c06cc614f2232cd9c668f8e
Denis Chaban <denisbrd8@gmail.com>
2025-03-09 13:06:51 +0700

    верный комит

--- ex15/src/main/java/ru/otus/hw/ex15/repositories/ChessFairRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.hw.ex15.repositories;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import ru.otus.hw.ex15.models.ChessFair;
-
-public interface ChessFairRepository extends ReactiveCrudRepository<ChessFair, Long> {
-
-
-}
