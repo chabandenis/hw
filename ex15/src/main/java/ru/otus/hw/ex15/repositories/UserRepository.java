536fb44b68a6dfc96a249dcdf517d995480d6960
Denis Chaban <denisbrd8@gmail.com>
2025-03-09 12:43:59 +0700

    верный комит

--- ex15/src/main/java/ru/otus/hw/ex15/repositories/UserRepository.java
+++ ex15/src/main/java/ru/otus/hw/ex15/repositories/UserRepository.java
@@ -1,12 +1,15 @@
 package ru.otus.hw.ex15.repositories;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.otus.hw.ex15.models.User;
 
 import java.util.Collection;
 
+
+@RepositoryRestResource(path = "person")
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
     Flux<User> findByIdIn(Collection<Long> ids);
 
