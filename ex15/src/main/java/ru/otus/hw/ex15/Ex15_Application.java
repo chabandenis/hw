f300357d8cc20fa924c8208ad777092a15a8c9ea
Denis Chaban <denisbrd8@gmail.com>
2025-03-11 17:04:23 +0700

    верный комит

--- ex15/src/main/java/ru/otus/hw/ex15/Ex15_Application.java
+++ ex15/src/main/java/ru/otus/hw/ex15/Ex15_Application.java
@@ -1,16 +1,18 @@
 package ru.otus.hw.ex15;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Slf4j
 @SpringBootApplication
 @EnableWebMvc
 public class Ex15_Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Ex15_Application.class, args);
-        System.out.println("ресты");
+        log.debug("ресты");
     }
 
 }
