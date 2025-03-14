8b9c43e7bd229b4eae3d1f826eb4632517cdb98e
Denis Chaban <denisbrd8@gmail.com>
2025-03-14 13:43:03 +0700

    верный комит

--- ex17/ex17_user/src/main/java/ru/otus/hw/ex17/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.hw.ex17.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
